5e173ca2709f58c424e7f8746f7183202a185d64
wjm13206 <abc82864d@gmail.com>
2025-05-27 14:11:32 -0300

    更新地址，移除日语

--- userscript.js
+++ userscript.js
@@ -2,19 +2,17 @@
 // @name                GitHub Internationalization
 // @name:zh-CN          GitHub汉化插件
 // @name:ja             GitHub日本語
-// @namespace           https://github.com/k1995/github-i18n-plugin/
+// @namespace           https://github.com/wjm13206/github-i18n-plugin/
 // @version             0.31
 // @description         Translate GitHub.com
-// @description:zh      GitHub汉化插件，包含人机翻译
-// @description:zh-CN   GitHub汉化插件，包含人机翻译
-// @description:ja      GitHub日本語プラグイン
+// @description:zh      GitHub汉化插件
+// @description:zh-CN   GitHub汉化插件
 // @author              k1995
 // @match               https://github.com/*
 // @match               https://gist.github.com/*
 // @grant               GM_xmlhttpRequest
 // @grant               GM_getResourceText
-// @resource            zh-CN https://www.github-zh.com/raw-githubusercontent/k1995/github-i18n-plugin/master/locales/zh-CN.json?v=20240617
-// @resource            ja https://www.github-zh.com/raw-githubusercontent/k1995/github-i18n-plugin/master/locales/ja.json
+// @resource            zh-CN https://raw.githubusercontent.com/wjm13206/github-cn-plugin/master/locales/zh-CN.json
 // @require             https://cdnjs.cloudflare.com/ajax/libs/timeago.js/4.0.2/timeago.min.js
 // @require             https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js
 // @license MIT
